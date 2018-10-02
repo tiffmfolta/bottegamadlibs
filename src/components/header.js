17e17ef661d271f063df279c12daf32a6a1e0d63
Tiffany <tiffany.davis.6.25@gmail.com>
2018-10-02 16:57:09 -0600

    added some styles

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="header">
           <h1>Bottega Madlibs</h1>
-          <p> {content}</p>
+          <p></p>
         </div>
     )
 }
--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Input = (title) => {
-    render() {
         return (
             <div className="input">
-                <input> 
+                <input /> 
                 <label>{title}</label>
             </div>
         )
     }
-}
 
 export default Input;
