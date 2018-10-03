bed8d8fcafca6f966edc1a1808d747a8147ebccd
Tiffany <tiffany.davis.6.25@gmail.com>
2018-10-03 12:11:20 -0600

    added content visible state and generate and clear buttons with functionality

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
     
@@ -8,36 +9,76 @@ class Card extends Component {
         super()
 
         this.state = {
-            color: 'BLUE',
-            pluralNoun: ''
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+            nounTwo: '',
+            adjectiveThree: '',
+            celebTwo: '',
+            celebThree: '',
+            adjectiveFour: '',
+            nounThree: '',
+            celebFour: '',
+            adjectiveFive: '',
+            contentVisible: false
         }
-    }
-    
-    handleInputChange() {
-        this.setState({ color: 'red' })
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
     
     handleInputChange(event) {
-        this.setState({ color: event.target.value })
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    
+    handleFormSubmit(event) {
+        console.log('trying to handle form submit');
+        event.preventDefault()
+        this.setState({ contentVisible: !this.state.contentVisible })
     }
 
     render() {
-
+        
+        
+            const inputData = [
+                {title: 'Color', state: this.state.color, name: 'color'},
+                {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
+                {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
+                {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
+    
+                {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
+                {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
+                {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
+                {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
+    
+                {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
+                {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
+                {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
+                {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
+    
+                {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
+                {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
+                {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
+                {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'}
+            ]
+        
         return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                <input/>
-                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
-            </div>
-        )
-
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                { Input('Color') }
-                { Input('Plural Noun') }
-            </div>
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
+                {
+                    inputData.map(data => Input( (data), this.handleInputChange)) 
+                }
+                </div>
+                <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
+                {
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
+                }
+                <Content data={this.state}/>
+            </form>
         )
     }
 }
--- src/components/input.js
+++ src/components/input.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Input = (title) => {
+const Input = ({title, state, name}, onChange) => {
         return (
-            <div className="input">
-                <input /> 
+            <div key={name} className="input">
+                <input name={name} value={state} onChange={onChange}/> 
                 <label>{title}</label>
             </div>
         )
